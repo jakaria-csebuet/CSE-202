74ca39fe43c9be3d7a6b3d4f0e473ff025933bce
Md. Jakaria <jakaria.dcc11@gmail.com>
2015-09-18 16:31:41 +0600

    ok

--- src/Author.java
+++ src/Author.java
@@ -2,5 +2,37 @@
  * Created by MD on 18/09/2015.
  */
 public class Author {
+    private String name ;
+    private String email ;
 
+    public Author (){
+
+    }
+
+    public Author(String name,String email){
+        this.name = name ;
+        this.email = email ;
+    }
+
+    public Author(String name){
+        this.name = name ;
+    }
+
+    public String getName(){
+        return this.name ;
+    }
+
+    public String getEmail(){
+        return this.email ;
+    }
+
+    public void setEmail(String email){
+        this.email = email ;
+    }
+
+    public void print(){
+        System.out.println(this.name + "at" + this.email);
+    } // this is a fau comment
 }
+
+
--- src/Book.java
+++ src/Book.java
@@ -2,4 +2,64 @@
  * Created by MD on 18/09/2015.
  */
 public class Book {
+    private String name ;
+    private Author author ;
+    private double price ;
+    private int stock ;
+
+    public Book(String name,Author author,double price,int stock){
+        this.name = name ;
+        this.author = author ;
+        this.price = price ;
+        this.stock = stock ;
+    }
+
+    public Book(String name,Author author){
+        this.name = name ;
+        this.author = author ;
+    }
+
+    public String getName(){
+        return this.name ;
+    }
+
+    public Author getAuthor(){
+        return author ;
+    }
+
+    public double getPrice(){
+        return this.price ;
+    }
+
+    public void setPrice(double price){
+        this.price = price ;
+    }
+
+    public int getStock(){
+        return this.stock ;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock ;
+    }
+
+    public void returnBook(int stock){
+        this.stock += stock ;
+    }
+
+    public void borrowBook(int stock){
+        if(this.stock >= stock)
+            this.stock -= stock ;
+        else
+            System.out.println("Borrowing amount exceeds the current stock!");
+    }
+
+    public String getAuthorName(){
+        return author.getName() ;
+    }
+
+    public void print(){
+        System.out.println("Author: " + getAuthorName() + " Name: " + this.name + " Price : " + this.price + " Stock: " + this.stock + '\n') ;
+    }
+
 }
